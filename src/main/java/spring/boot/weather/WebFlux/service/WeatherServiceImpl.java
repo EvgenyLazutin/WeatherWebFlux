411d5e3befae6ed1ea423d1a87c4cdc37ee1e438
elazutin <e_lazutin@mail.ru>
2020-01-31 17:28:59 +0300

    Add JSON wrapper.

--- src/main/java/spring/boot/weather/WebFlux/service/WeatherServiceImpl.java
+++ src/main/java/spring/boot/weather/WebFlux/service/WeatherServiceImpl.java
@@ -1,6 +1,7 @@
 package spring.boot.weather.WebFlux.service;
 
 
+import com.fasterxml.jackson.databind.ObjectMapper;
 import org.springframework.stereotype.Service;
 import org.springframework.web.reactive.function.client.WebClient;
 import reactor.core.publisher.Mono;
@@ -20,5 +21,10 @@ public class WeatherServiceImpl implements WeatherService{
     public Mono<City> weatherByNameCity(String cityName, String apiKey) {
         return this.webClient.get().uri("/data/2.5/weather?q={cityName}&appid={apiKey}", cityName, apiKey)
                 .retrieve().bodyToMono(City.class);
+    }
+
+    private  <T> T readJsonFile(String fileName, Class<T> responseClass) throws Exception {
+        ObjectMapper mapper = new ObjectMapper();
+        return mapper.readValue(getClass().getResourceAsStream("/" + fileName), responseClass);
     }
 }
